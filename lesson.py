3a0dc12d986ef64840f0d8653107c562c7c3ddf3
DESKTOP-4R0N8H5\Andrey <belousovandrey3105@gmail.com>
2023-04-11 00:04:24 +0300

    add login

--- lesson.py
+++ lesson.py
@@ -1,6 +1,7 @@
 import time
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -10,5 +11,20 @@ from webdriver_manager.chrome import ChromeDriverManager
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 driver.get('https://www.saucedemo.com/')
 driver.maximize_window()
-time.sleep(1)
-driver.close()
+user_name = driver.find_element(By.ID,'user-name')
+# user_name = driver.find_element(By.NAME,'user-name')
+# user_name = driver.find_element(By.XPATH,'//*[@id="user-name"]')
+# user_name = driver.find_element(By.CSS_SELECTOR,'[data-test="username"]')
+password = driver.find_element(By.CSS_SELECTOR,'#password')
+button_login = driver.find_element(By.CSS_SELECTOR, '#login-button')
+user_name.send_keys('standard_user')
+password.send_keys('secret_sauce')
+button_login.click()
+
+
+
+
+
+
+time.sleep(3)
+# driver.close()
